07ed9572c7047dd95c21a1b9fc2bdc3f741ec9ad
Daniel Chiquito <daniel.chiquito@kitware.com>
2022-02-11 18:56:13 -0500

    test-app

---
+++ test-app/test_app/core/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    name = 'test_app.core'
+    verbose_name = 'Girder 4 Pytest Pyppeteer Test App: Core'
---
+++ test-app/test_app/settings.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from composed_configuration import (
+    ComposedConfiguration,
+    ConfigMixin,
+    DevelopmentBaseConfiguration,
+    HerokuProductionBaseConfiguration,
+    ProductionBaseConfiguration,
+    TestingBaseConfiguration,
+)
+
+
+class TestAppMixin(ConfigMixin):
+    WSGI_APPLICATION = 'test_app.wsgi.application'
+    ROOT_URLCONF = 'test_app.urls'
+
+    BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+    @staticmethod
+    def mutate_configuration(configuration: ComposedConfiguration) -> None:
+        # Install local apps first, to ensure any overridden resources are found first
+        configuration.INSTALLED_APPS = [
+            'test_app.core.apps.CoreConfig',
+        ] + configuration.INSTALLED_APPS
+
+        # Install additional apps
+        configuration.INSTALLED_APPS += [
+            's3_file_field',
+        ]
+
+
+class DevelopmentConfiguration(TestAppMixin, DevelopmentBaseConfiguration):
+    pass
+
+
+class TestingConfiguration(TestAppMixin, TestingBaseConfiguration):
+    pass
+
+
+class ProductionConfiguration(TestAppMixin, ProductionBaseConfiguration):
+    pass
+
+
+class HerokuProductionConfiguration(TestAppMixin, HerokuProductionBaseConfiguration):
+    pass

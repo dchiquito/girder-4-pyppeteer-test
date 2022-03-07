9d30f2f1df361805af3119d7556a06048b195bde
Daniel Chiquito <daniel.chiquito@kitware.com>
2022-03-07 15:12:52 -0500

    Log node/npm/yarn versions

--- execute-test-command.sh
+++ execute-test-command.sh
@@ -4,10 +4,12 @@ echo "Navigating to $INPUT_WORKING_DIRECTORY"
 cd $INPUT_WORKING_DIRECTORY
 echo "Installing with $INPUT_INSTALL_COMMAND"
 $INPUT_INSTALL_COMMAND
-echo "Currently in:"
-pwd
-echo "Contents:"
-ls
+echo "node version:"
+node --version
+echo "npm version:"
+npm --version
+echo "yarn version:"
+yarn --version
 echo "Running tests with $INPUT_TEST_COMMAND"
 $INPUT_TEST_COMMAND
 echo "Complete!"

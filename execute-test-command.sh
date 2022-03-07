deaa83e5cc2cf98bfe715a4227b4260ef0175045
Daniel Chiquito <daniel.chiquito@kitware.com>
2022-03-07 12:00:29 -0500

    Adding debug stuff to docker entrypoint

--- execute-test-command.sh
+++ execute-test-command.sh
@@ -2,6 +2,12 @@ echo "Navigating to $INPUT_WORKING_DIRECTORY"
 cd $INPUT_WORKING_DIRECTORY
 echo "Installing with $INPUT_INSTALL_COMMAND"
 $INPUT_INSTALL_COMMAND
+echo "Doing git stuff"
+pwd
+ls
+cd $GITHUB_WORKSPACE
+pwd
+ls
 echo "Running tests with $INPUT_TEST_COMMAND"
 $INPUT_TEST_COMMAND
 echo "Complete!"

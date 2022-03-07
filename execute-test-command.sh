23e40db456689a5202e977d121dc911f0f7d360a
Daniel Chiquito <daniel.chiquito@kitware.com>
2022-03-07 12:04:30 -0500

    cd into test directory

--- execute-test-command.sh
+++ execute-test-command.sh
@@ -1,12 +1,12 @@
+echo "Doing git stuff"
+cd $GITHUB_WORKSPACE
 echo "Navigating to $INPUT_WORKING_DIRECTORY"
 cd $INPUT_WORKING_DIRECTORY
 echo "Installing with $INPUT_INSTALL_COMMAND"
 $INPUT_INSTALL_COMMAND
-echo "Doing git stuff"
+echo "Currently in:"
 pwd
-ls
-cd $GITHUB_WORKSPACE
-pwd
+echo "Contents:"
 ls
 echo "Running tests with $INPUT_TEST_COMMAND"
 $INPUT_TEST_COMMAND

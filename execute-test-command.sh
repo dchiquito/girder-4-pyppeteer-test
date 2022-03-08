7d24315745661128e51df3e54c6f4f14123855c1
Daniel Chiquito <daniel.chiquito@kitware.com>
2022-03-08 10:28:15 -0500

    Manage directories more cleanly

--- execute-test-command.sh
+++ execute-test-command.sh
@@ -1,12 +1,19 @@
-echo "Doing git stuff"
+# Install the project
 cd $GITHUB_WORKSPACE
-echo "Navigating to $INPUT_WORKING_DIRECTORY"
-cd $INPUT_WORKING_DIRECTORY
+echo "Navigating to $INPUT_INSTALL_DIRECTORY"
+cd $INPUT_INSTALL_DIRECTORY
 echo "Installing with $INPUT_INSTALL_COMMAND"
 $INPUT_INSTALL_COMMAND
+
+# Log versions for debugging
 echo "node version: $(node --version)"
 echo "npm version: $(npm --version)"
 echo "yarn version: $(yarn --version)"
 echo "vue version: $(vue --version)"
+
+# Run the pyppeteer tests
+cd $GITHUB_WORKSPACE
+echo "Navigating to $INPUT_TEST_DIRECTORY"
+cd $INPUT_TEST_DIRECTORY
 echo "Running tests with $INPUT_TEST_COMMAND"
 $INPUT_TEST_COMMAND
